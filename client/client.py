647b7a18e41dd1b43d7442bd442ab522283fb107
Christopher Budo <budocf@rose-hulman.edu>
2017-03-29 16:23:36 -0700

    moved code into individual functions for iWant and uTake

--- client/client.py
+++ client/client.py
@@ -1,28 +1,24 @@
 from socket import *
-serverName = 'localhost'
-serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = raw_input('Input command:')
-if 'iWant' in sentence:
-    #send to server
-    clientSocket.send(sentence)
-    file = open('receive/'+sentence.split(' ')[1], 'wb')
+def iWant(clientSocket, command):
+    clientSocket.send(command)
+    file = open('receive/'+command.split(' ')[1], 'wb')
     l = clientSocket.recv(1024)
     if l == 'Failure: What you talkin\' bout Willis? I ain\'t seen that file nowhere!':
         print(l)
         l = None
-    while l:
+    lastTime = False
+    while l and not lastTime:
         if 'EOF' in l:
-            lastTime = true
+            lastTime = True
             l = l[:l.index('EOF')]
         file.write(l)
         l = clientSocket.recv(1024)
     file.close()
-elif 'uTake' in sentence:
+
+def uTake(clientSocket, command):
     #send file to server
-    clientSocket.send(sentence+';')
-    filename = sentence.split(' ')[1]
+    clientSocket.send(command+';')
+    filename = command.split(' ')[1]
     file = open(filename, 'rb')
     l = file.read(1024)
     while l:
@@ -30,9 +26,21 @@ elif 'uTake' in sentence:
         l = file.read(1024)
     file.close()
     clientSocket.shutdown(SHUT_WR)
-else:
-    #error
-    print('Failure: What you talkin\' bout Willis?')
-modifiedSentence = clientSocket.recv(1024)
-print('From Server:', modifiedSentence)
-clientSocket.close()
+
+if __name__ == '__main__':
+    serverName = 'localhost'
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+    command = raw_input('Input command:')
+    if 'iWant' in command:
+        #send to server
+        iWant(clientSocket,command)
+    elif 'uTake' in command:
+        uTake(clientSocket, command)
+    else:
+        #error
+        print('Failure: What you talkin\' bout Willis?')
+    modifiedcommand = clientSocket.recv(1024)
+    print('From Server:', modifiedcommand)
+    clientSocket.close()
--- server/server.py
+++ server/server.py
@@ -1,38 +1,50 @@
 from socket import *
 import os.path
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('',serverPort))
-serverSocket.listen(1)
-print('The server is ready to receive')
-while 1:
-    connectionSocket, addr = serverSocket.accept()
 
-    sentence = connectionSocket.recv(1024)
-    if sentence.split(' ')[0] == 'uTake':
-        #accept sent file
-        file = open('store/' + sentence.split(' ')[1].split(';')[0], 'wb')
-        l = sentence.split(';')[1]
-        while l:
-            file.write(l)
-            l = connectionSocket.recv(1024)
-        file.close()
-        print('recieved file {0}'.format(sentence.split(' ')[1]))
-    elif sentence.split(' ')[0] == 'iWant':
-        #find file or say file not found
-        if not os.path.isfile(sentence.split(' ')[1]):
-            connectionSocket.send('Failure: What you talkin\' bout Willis? I ain\'t seen that file nowhere!')
+def uTake(connectionSocket, command):
+    filename = command.split(';')[0].split('/')[-1]
+    file = open('store/' + filename, 'wb')
+    l = command.split(';')[1]
+    while l:
+        file.write(l)
+        l = connectionSocket.recv(1024)
+    file.close()
+    print('received file {0}'.format(filename))
+
+def iWant(connectionSocket, command):
+    print(command)
+    file = open('store/' + command, 'rb')
+    l = file.read(1024)
+    while l:
+        print(l)
+        connectionSocket.send(l)
+        l = file.read(1024)
+    file.close
+    connectionSocket.send('EOF')
+    connectionSocket.shutdown(SHUT_WR)
+
+
+if __name__ == '__main__':
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('',serverPort))
+    serverSocket.listen(1)
+    print('The server is ready to receive')
+    while 1:
+        connectionSocket, addr = serverSocket.accept()
+
+        sentence = connectionSocket.recv(1024)
+        if sentence.split(' ')[0] == 'uTake':
+            #accept sent file)
+            uTake(connectionSocket, sentence.split(' ')[1])
+        elif sentence.split(' ')[0] == 'iWant':
+            #find file or say file not found
+            if not os.path.isfile(sentence.split(' ')[1]):
+                connectionSocket.send('Failure: What you talkin\' bout Willis? I ain\'t seen that file nowhere!')
+            else:
+                iWant(connectionSocket, sentence.split(' ')[1])    
         else:
-            file = open('store/' + sentence.split(' ')[1], 'rb')
-            l = file.read(1024)
-            while l:
-                connectionSocket.send(l)
-                l = file.read(1024)
-            file.close
-            connectionSocket.send('EOF')
-        print('not implemented')
-    else:
-        #error
-        print('error')
-    connectionSocket.send('Thank you for using dickbut')
-    connectionSocket.close()
+            #error
+            print('error')
+        connectionSocket.send('Thank you for using dickbut')
+        connectionSocket.close()
